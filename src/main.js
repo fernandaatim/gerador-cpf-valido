2e00da7e2b61ad9dbf6179a5787b8153a9f250c5
fernandaatim <143547756+fernandaatim@users.noreply.github.com>
2025-02-06 09:25:58 -0300

    :tada:First Commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import GeraCPF from './modules/GeraCPF'
+import './assets/css/main.css'
+
+(function(){
+    document.querySelector('button').addEventListener('click', e => {
+        e.preventDefault();
+        const gera = new GeraCPF();
+        const cpfGerado = document.querySelector('.cpf-gerado');
+    
+        cpfGerado.innerHTML = gera.geraNovoCpf();
+    });
+})();
